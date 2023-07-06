e88516d42cfa8728c87906253c1eb21424533fb6
KimSoYeon <soyeon001117@gmail.com>
2023-07-06 20:51:30 +0900

    UI 변경

--- lib/src/view/home.dart
+++ lib/src/view/home.dart
@@ -20,11 +20,11 @@ class HomeView extends GetView<HomeController> {
         () => Column(
           children: [
             _date(),
-            SizedBox(
-              height: 20,
+            const SizedBox(
+              height: 40,
             ),
             _create(),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             _todoList(),
@@ -39,13 +39,21 @@ class HomeView extends GetView<HomeController> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          width: 250,
-          child: TextField(
-            controller: controller.createController,
+          width: 300,
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(16.0)),
+            child: TextField(
+              controller: controller.createController,
+            ),
           ),
         ),
         ElevatedButton(
-            onPressed: controller.create, child: const Icon(Icons.send))
+            onPressed: controller.create, child: const Icon(Icons.add))
       ],
     );
   }
@@ -56,28 +64,45 @@ class HomeView extends GetView<HomeController> {
         itemCount: controller.todos.length,
         itemBuilder: (context, index) {
           final todoModel = controller.todos[index];
-          return ListTile(
-            leading: (todoModel.isDone!)
-                ? GestureDetector(
-                    onTap: () {
-                      controller.deleteTodo(todoModel.id!);
-                    },
-                    child: const Icon(
-                      Icons.check,
-                      color: Colors.green,
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: SizedBox(
+              height: 60,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2.0,
                     ),
-                  )
-                : GestureDetector(
-                    onTap: () {
-                      controller.updateTodo(todoModel.id!);
-                    },
-                    child: const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
+                    borderRadius: BorderRadius.circular(16.0)),
+                child: ListTile(
+                  trailing: (todoModel.isDone!)
+                      ? GestureDetector(
+                          onTap: () {
+                            controller.deleteTodo(todoModel.id!);
+                          },
+                          child: const Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            controller.updateTodo(todoModel.id!);
+                          },
+                          child: const Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ),
+                        ),
+                  title: Text(
+                    todoModel.todo,
+                    textAlign: TextAlign.center,
                   ),
-            title: Text(todoModel.todo),
-            subtitle: Text(todoModel.time.toString()),
+                ),
+              ),
+            ),
           );
         },
       ),
